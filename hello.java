1716927b9606f372b3fcac799254c75aaff1ec8e
Mayur Hole <mayur.hole@joshsoftware.com>
2022-07-20 14:31:33 +0530

    Revert "reverting"
    This reverts commit 9a5845e7636a53ab0bdc4ee4b15831a64f3d423e.

--- hello.java
+++ hello.java
@@ -4,7 +4,6 @@ public class hello
 {
 	public static void main(String args[])
 	{	
-		System.out.println("Hello World! Adding changes to file");
-		System.out.println("third change");
+		System.out.println("Hello World! Adding chamges to file");
 	}
 }
